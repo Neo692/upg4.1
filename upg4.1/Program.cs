6081356d65a2caead11b85ae7f1fbb2548dda503
nehy06001 <nehy06001@E-PW05CGQV>
2024-12-10 13:50:34 +0100

    Add project files.

---
+++ upg4.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace upg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 30 ; i++)
+            {
+                Console.WriteLine(i);
+            }
+            {
+                for (int j = 200; j >= 180; j--)
+                {
+                    Console.WriteLine(j);
+                }
+                {
+                    for (int k = 1000; k <= 1400; k++)
+                    {
+                        Console.WriteLine(k);
+                    }
+                }
+            }
+        }
+    }
+}
